e365910deb2fe662d7cfb6552878c4cf11347608
Eric Cote <EricCote@ReactAcademy.ca>
2021-01-05 19:35:32 +0000

    send the code

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,33 @@ import Counter from './Counter/Counter';
 // import YouTube  from './Components/YouTube';
 
 
-export function App() {
+function Routing(props) {
+  switch (props.page) {
+    case '#home':
+      return <Home />;
+    case '#about':
+      return <About />;
+    case '#counter':
+      return <Counter />;
+    default:
+      return <Home />;
+  }
+}
 
+export function App() {
+  const [currentPage, setCurrentPage] = React.useState(window.location.hash);
+
+  React.useEffect(() => {
+    window.addEventListener("hashchange",
+      () => { setCurrentPage(window.location.hash) }
+    )
+  }, [])
 
   return (
     <>
       <Menu />
       <Container className='my-4'>
-        <Home />
-        <About />
-        <Counter />
+        <Routing page={currentPage} />
         <Footer />
       </Container>
     </>
