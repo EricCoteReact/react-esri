2d813b06090407ac9d8030d36d3749fd8e11574f
Eric Cote <EricCote@ReactAcademy.ca>
2021-01-05 20:46:56 +0000

    changes to "about" page for a list of authors

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { Table } from 'reactstrap';
 
 export default function About() {
+  const authors = [
+    { name: "Eric", id: 1 },
+    { name: "Jason", id: 2 },
+    { name: "Mark", id: 3 },
+  ];
+
   return (
     <div>
       <h1>About</h1>
       <p>This is the about page</p>
+      <Table striped dark>
+        <tbody>
+          {authors.map((author) =>
+            <tr key={author.id}>
+              <td>{author.id}</td>
+              <td>{author.name}</td>
+            </tr>)}
+        </tbody>
+      </Table>
     </div>
   )
 }
